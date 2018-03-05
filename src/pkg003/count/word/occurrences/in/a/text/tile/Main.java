2307f9d22572b0f8d908bfb434dc536b1a5cdf58
Karpowicz <Emil.Karpowicz@unilever.com>
2018-03-05 17:49:26 +0100

    Create main view divided into 3 panels

--- src/pkg003/count/word/occurrences/in/a/text/tile/Main.java
+++ src/pkg003/count/word/occurrences/in/a/text/tile/Main.java
@@ -1,15 +1,68 @@
 package pkg003.count.word.occurrences.in.a.text.tile;
+
+import java.awt.*;
+import javax.swing.*;
+
 /**
  * Pro/g/ramming Challenges v 3.0
  * 003 - Count word occurrences in a text tile
  * @author Emil.Karpowicz
  */
 
-public class Main 
+public class Main extends JFrame 
 {
-
-    public static void main(String[] args) 
+    private static double sWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+    private static double sHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+    
+    private static int cubit = 100;
+    
+    private JPanel mainPanel = new JPanel();
+    private JPanel textPanel = new JPanel();
+    private JTextArea textArea = new  JTextArea();
+    private JScrollPane scrollTextArea = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    private JPanel outcomePanel = new JPanel();
+    private JPanel controlPanel = new JPanel();
+    
+    private CustomButton uploadFileButton = new CustomButton("Upload File");
+    private CustomButton countWordOccurance = new CustomButton("Count Occurance");
+    
+    public Main()
     {
+        this.setTitle("PCv3.0: 003 - Count word occurrences in a text tile");
+        this.setBounds((int)((sWidth - 8*cubit)/ 2), (int)((sHeight-5*cubit) / 2), 8*cubit, 5*cubit);
+        this.getContentPane().add(mainPanel);
+            mainPanel.setLayout(new BorderLayout());
+            outcomePanel.setBorder(BorderFactory.createBevelBorder(0));
+            outcomePanel.setPreferredSize(new Dimension(this.getWidth(), cubit / 2));
+            mainPanel.add(outcomePanel, BorderLayout.NORTH);
+            scrollTextArea.setPreferredSize(new Dimension(this.getWidth(), this.getHeight()));
+            mainPanel.add(scrollTextArea, BorderLayout.CENTER);
+            controlPanel.setBorder(BorderFactory.createBevelBorder(0));
+            controlPanel.setPreferredSize(new Dimension(this.getWidth(), cubit / 2));
+            mainPanel.add(controlPanel, BorderLayout.SOUTH);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
+    public static void main(String[] args) 
+    {
+        new Main().setVisible(true);
+    }
+    
+}
+
+class CustomButton extends JButton
+{
+    public CustomButton(String buttonName)
+    {
+        super(buttonName);
+    }
+}
+
+class CustomLabel extends JLabel
+{
+    public CustomLabel(String labelText)
+    {
+        super(labelText);
+    }
 }
